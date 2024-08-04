d226a055e93df8e7e24fde41e67a7f80bcf0d061
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-04 09:36:08 -0500

    class App: ejemplos con TreeSet

--- src/App.java
+++ src/App.java
@@ -6,12 +6,31 @@ import com.jhonatan.ejemplos.modelo.Alumno;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ejemploAlumnosTreeSet();
+        // ejemploAlumnosTreeSet();
+        ejemploAlumnosTreeSetComparto();
     }
 
     public static void ejemploAlumnosTreeSet() {
         System.out.println("\t==TreeSet de Alumnos==");
         Set<Alumno> sa = new TreeSet<>();
+        sa.add(new Alumno("Pato", 5));
+        sa.add(new Alumno("Cata", 6));
+        sa.add(new Alumno("Luci", 4));
+        sa.add(new Alumno("Jano", 7));
+        sa.add(new Alumno("Andrez", 3));
+        sa.add(new Alumno("Zeus", 2));
+
+        System.out.println(sa);
+    }
+
+    public static void ejemploAlumnosTreeSetComparto() {
+        System.out.println("\t==TreeSet de Alumnos==");
+
+        // se pude cambiar el orden en que queremos ordenar
+        // Set<Alumno> sa = new TreeSet<>((a, b) ->
+        // (b.getNombre().compareTo(a.getNombre())));
+        Set<Alumno> sa = new TreeSet<>((a, b) -> (b.getNota().compareTo(a.getNota())));
+
         sa.add(new Alumno("Pato", 5));
         sa.add(new Alumno("Cata", 6));
         sa.add(new Alumno("Luci", 4));
